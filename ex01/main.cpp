cb54f113c6c6323d82ccf90cb0aa69d0b05ff639
Matteo Marinelli <mmarinel@e3r6p6.42roma.it>
2023-04-05 13:55:25 +0200

    RPN: written

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mmarinel <mmarinel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 22:39:35 by mmarinel          #+#    #+#             */
-/*   Updated: 2023/03/31 13:12:45 by mmarinel         ###   ########.fr       */
+/*   Updated: 2023/04/05 13:51:22 by mmarinel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,13 @@
 # include <stack>
 # include <cctype>
 # include <iostream>
+
+bool	is_operator(const char c)
+{
+	return (
+		c == '+' || c == '-' || c == '*' || c == '/'
+	);
+}
 
 void	operator_apply(std::stack<std::string>& stack, char op)
 {
@@ -42,30 +49,29 @@ int main( int argc, const char* argv[])
 	if (2 == argc)
 	{
 		std::stack<std::string>	stack;
-		int				i;
+		int						i;
 
 		i = 0;
 		while (argv[1][i])
 		{
 			if (std::isdigit(argv[1][i]))
 			{
-				std::cout << "is digit" << std::endl;
-				// exit(0);
 				stack.push(std::string(1, argv[1][i]));
 			}
-			else
+			else if (is_operator(argv[1][i]))
 			{
-				std::cout << "is opr" << std::endl;
-				// exit(0);
 				operator_apply(stack, argv[1][i]);
 			}
+			else if (false == std::isspace(argv[1][i]))
+			{
+				std::cout << "Error: wrong symbol found" << std::endl;
+				exit(1);
+			}
 			i++;
-			std::cout << "iteration n• " << i << std::endl;
 		}
 		std::cout << stack.top() << std::endl;
 	}
 	else
 		std::cout << "Error: Wrong number of arguments" << std::endl;
-	std::cout << std::endl;
 	return 0;
 }
